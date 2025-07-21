40dc00d47720955c05ac61c67eacb4e482fab6ca
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-07-21 00:02:59 -0400

    fix: lint

--- src/env.ts
+++
@@ -1,8 +0,0 @@
-import { getEnv } from "complete-node";
-import { z } from "zod";
-
-const envSchema = z.object({
-  GEMINI_API_KEY: z.string().min(1),
-});
-
-export const env = await getEnv(envSchema);
